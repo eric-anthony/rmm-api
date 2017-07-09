bdeff2d71f9ce97ac30656a50c5b90c5f9e2d73f
Eric Anthony <eric@esanthony.com>
2017-07-09 20:02:30 +0000

    First Commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+
+app = Flask(__name__)
+
+app.secret_key = 'qwertylkjhgzxcvb'
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/')
+@login_required
+def home():
+    return render_template("index.html")
+    
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+    
+@app.route('/login',methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials - Please try again'
+        else:
+            session['logged_in'] = True
+            flash('You were just logged in')
+            return redirect(url_for('home'))
+    
+    return render_template("login.html", error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out')
+    return redirect(url_for('welcome'))
+
+if __name__ == '__main__':
+    host = os.getenv('IP','0.0.0.0')
+    port = int(os.getenv('PORT',5000))
+    app.debug = True
+    app.run(host=host,port=port)
+    
